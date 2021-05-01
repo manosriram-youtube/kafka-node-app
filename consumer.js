ee5108f93d4ec832f2271c936c061c7ec86a27dd
Mano Sriram <mano.sriram0@gmail.com>
2021-05-01 10:31:56 +0530

    Push 3 files

---
+++ consumer.js
@@ -0,0 +1,31 @@
+const { Kafka } = require("kafkajs");
+
+async function consume() {
+    const kafka = new Kafka({
+        clientId: "player-jersey-1",
+        brokers: ["127.0.0.1:9092"],
+    });
+
+    const consumer = kafka.consumer({ groupId: "player-jersey" });
+    await consumer.connect();
+    console.log("Consumer connected");
+
+    await consumer.subscribe({
+        topic: "jersey1",
+        fromBeginning: true,
+    });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            // 1. topic
+            // 2. partition
+            // 3. message
+
+            console.log(
+                `To Partition ${partition} -> message ${message.value.toString()}`
+            );
+        },
+    });
+}
+
+consume();
